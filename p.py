1263d8094928567e9548250618b94803d0bca526
Sushmitamaha27 <sushmitagamahajan@gmail.com>
2023-03-28 23:06:45 +0530

    initial commit

---
+++ p.py
@@ -0,0 +1,15 @@
+lst=[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+d=[1,2,3,4]
+for i in lst[:4]:
+    empty=i
+if lst[:4]==d:
+        print("true")
+else:
+        print("false")   
+if lst[4:8]==d:
+        print("true")
+else:
+        print("false")   
+ 
+ 
+        
---
+++ prac.py
@@ -0,0 +1,14 @@
+# user=int(input("enter the number"))
+# if user>4**4 and user%34==4:
+#     print("true") 
+# else:
+#     print("false")
+
+
+from array import *
+
+arr=array('i',[1, 3, 5, 7, 9])
+print(arr.buffer_info())
+
+
+
