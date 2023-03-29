62451ed43f783d6339eef8062c17d9be41657a7d
Sushmitamaha27 <sushmitagamahajan@gmail.com>
2023-03-29 20:49:06 +0530

    initial comment

--- p.py
+++ p.py
@@ -1,15 +1,22 @@
-lst=[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
-d=[1,2,3,4]
-for i in lst[:4]:
-    empty=i
-if lst[:4]==d:
-        print("true")
-else:
-        print("false")   
-if lst[4:8]==d:
-        print("true")
-else:
-        print("false")   
+# lst=[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]
+# d=[1,2,3,4]
+# for i in lst[:4]:
+#     empty=i
+# if lst[:4]==d:
+#         print("true")
+# else:
+#         print("false")   
+# if lst[4:8]==d:
+#         print("true")
+# else:
+#         print("false")   
  
  
-        
+# def num(abc_list,num_list):
+#     for char in abc_list:
+#         for num in num_list:
+#             print(char,num)
+#     return
+
+# num(['a','b','c'],[1,2,3])
+
